b5e9913d6e2e08ff4fca9a8a6b93db1944bbe568
Алексей Стебло <asteblo@gmail.com>
2019-12-30 22:21:23 +0300

    SQL

--- block_2_3.sql
+++ block_2_3.sql
@@ -3,8 +3,8 @@
 
 CREATE TEMPORARY TABLE TEMP ( name VARCHAR(255) NOT NULL, count INT(255) NOT NULL);
 INSERT INTO TEMP
-SELECT `name`, COUNT(`id`) FROM `days_of_week`
-JOIN `work_day` ON (`days_of_week`.`id` = `work_day`.`id_day`)  
+SELECT `name`, COUNT(`id_day`) FROM `days_of_week`
+LEFT JOIN `work_day` ON (`days_of_week`.`id` = `work_day`.`id_day`)  
 GROUP BY `name`;
 SELECT @maxim := MIN(count) FROM TEMP;
 SELECT `name` FROM TEMP WHERE count = @maxim;
